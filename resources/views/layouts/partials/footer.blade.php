10da2c9f485f65991b734f47cea7e648d631790b
Jpeyl <jhonpaulbarit1@gmail.com>
2024-12-11 07:28:05 +0800

    first commit

---
+++ resources/views/layouts/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="float-right d-none d-sm-block">
+      Developed By Minsu IT
+    </div>
+    <strong>&copy; <?php echo date('Y')?> - Restaurant POS</strong> - Minsu
+  </footer>
